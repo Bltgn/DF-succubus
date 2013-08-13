1711d131920f7caa80d8e1545b0160283cb69b46
Guillaume Olivetti <contact@devduweb.com>
2013-08-13 08:27:45 +0200

    script - untame fix

--- fooccubus-prod/hack/scripts/fooccubus-untame.lua
+++ fooccubus-prod/hack/scripts/fooccubus-untame.lua
@@ -1,31 +1,32 @@
 -- Remove the tame flag from soul wisps within LOS
 local fov = require 'fov'
 local utils = require 'utils'
-
+ 
 if not dfhack.isMapLoaded() then qerror('Map is not loaded.') end
-
+if not ... then qerror('Please enter a creature ID.') end
+ 
 local unit = nil
 local unitList = df.global.world.units.active
 local unitId = tonumber(...)
-
+ 
 -- Finding the source unit
 function findUnit(searchId)
 	local k, _
-
+ 
 	for k, _ in ipairs(unitList) do
 		if unitList[k].id == searchId then
 			return unitList[k]
 		end
 	end
-
+ 
 	return nil
 end
-
+ 
 -- Check if the unit is seen
 function isSeen(view, p)
-	return p.z == view.z and view[p.y] and view[p.y][p.x] > 0
+	return view.z and p.z == view.z and view[p.y] and view[p.y][p.x] > 0
 end
-
+ 
 -- Find soul wisps within the LOS of the creature
 function findLos(unitSource)
 	local radius = 5
@@ -41,12 +42,12 @@ function findLos(unitSource)
 		end
 	end
 end
-
+ 
 function untame(unit)
 	unit.flags1.tame = false
 end
-
+ 
 unit = findUnit(unitId)
 if not unit then qerror('Unit not found.') end
-
+ 
 findLos(unit)
