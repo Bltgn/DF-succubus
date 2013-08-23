7bc68549f30a2bb68709628d49ca7c5221d7101d
Guillaume Olivetti <contact@devduweb.com>
2013-08-23 16:20:47 +0200

    lua update

--- init.lua
+++ init.lua
@@ -2,4 +2,6 @@
 -- This file will run fixes and tweaks when you load your saves
 
 -- Fix the summoned creatures skills
-dfhack.run_script('fooccubus-skills')
+if dfhack.isMapLoaded() then
+	dfhack.run_script('fooccubus-skills')
+end
