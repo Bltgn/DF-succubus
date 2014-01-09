e1bdd5a5de109f106fa8d3e923736fd9c48d08b1
Guillaume Olivetti <contact@devduweb.com>
2014-01-09 08:26:53 +0100

    reaction - added rain and broker skill

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
 -- Succubus fortress
 -- This file will run fixes and tweaks when you load your saves
 
--- Fix the summoned creatures skills
-if dfhack.isMapLoaded() then
-	dfhack.run_script('fooccubus-skills')
-end
