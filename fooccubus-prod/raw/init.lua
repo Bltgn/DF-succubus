070835e356d4ccc64a0f8a041a96008dc4eff287
U-Guillaume-Win\Guillaume <Guillaume@Guillaume-Win.(none)>
2014-01-17 19:58:09 +0100

    fixed init/lua

--- fooccubus-prod/raw/init.lua
+++ fooccubus-prod/raw/init.lua
@@ -1,5 +1,26 @@
 -- Succubus fortress
 -- This file will run fixes and tweaks when you load your saves
 
--- Fix the summoned creatures skills
-dfhack.run_script('fooccubus-skills')
+-- Make sure that commands are only run if you play as a succubus
+function isFooccubus()
+
+	for k, entity in pairs(df.global.world.raws.entities) do
+		if entity.code == 'CULT' or entity.code == 'DECADENCE' then return true end
+	end
+
+	return false
+
+end
+
+function onStateChange(sc)
+
+	if sc == SC_MAP_LOADED then
+
+		print('Map loaded')
+		if isFooccubus() then
+			dfhack.run_script('fixnakedregular')
+		end
+
+	end
+
+end
