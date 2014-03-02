f45d9356a8eec4d78bb18b587ab97ba8f3ddb7ce
Guillaume <contact@devduweb.com>
2014-03-02 15:22:41 +0100

    bugfixes

--- fooccubus-prod/hack/scripts/succubus/berserk.lua
+++ fooccubus-prod/hack/scripts/succubus/berserk.lua
@@ -1,28 +1,32 @@
--- Apply a syndrome depending of its unhappy rating
-
-local rating = require rating
+-- Apply a syndrome depending of its unhappy rating, it is run on the target creature and use addsyndrome to simulate a self buff
+local rating = require 'rating'
+local fov = require 'fov'
 
 if not dfhack.isMapLoaded() then qerror('Map is not loaded.') end
-if not ... then qerror('Please enter a creature ID.') end
 
 local args = {...}
-local unit, power
+local debug = true
+local syndrome, power, name
 
 if not args[1] then qerror('Berserk : No unit entered') end
 
-unit = df.unit.find(tonumber(args[1]))
+unit = df.unit.find(args[1])
+if not unit then qerror('Berserk : Unit not found.') end
+
 power = rating.unhappiness(unit)
 name = dfhack.units.getVisibleName(unit)
 
+if debug then print('Berserk unit #'..unit.id..' rating '..power) end
+
 -- Lets pick a syndrome tier
 if power < 3 then
-	-- Slight bonus
-	dfhack.run_script('addsyndrome', 'FOOCCUBUS_BERSERK_1', args[1], '-1, -1, -1', 'civ')
+	syndrome = 'FOOCCUBUS_BERSERK_1'
 elseif power < 5 then
-	-- Anger adding strenght
-	dfhack.run_script('addsyndrome', 'FOOCCUBUS_BERSERK_2', args[1], '-1, -1, -1', 'civ')
+	syndrome = 'FOOCCUBUS_BERSERK_2'
 else
-	-- Ouch!
 	dfhack.gui.showAnnouncement(name .. ' is consumed by anger!', COLOR_YELLOW)
-	dfhack.run_script('addsyndrome', 'FOOCCUBUS_BERSERK_3', args[1], '-1, -1, -1', 'civ')
+	syndrome = 'FOOCCUBUS_BERSERK_3'
 end
+
+dfhack.run_script('addsyndrome', syndrome, unit.id, '-1,-1,-1', 'civ', 'NONE', 'NONE', 'BERSERK', 'NONE', 'NONE', 'NONE', 'NONE', 'NONE')
+if debug then print('- Apply '..syndrome) end
