442c2a517bbf2da12c7912f051e2d6243db79c8c
Guillaume Olivetti <contact@devduweb.com>
2014-03-14 08:49:46 +0100

    interaction - added lust secret

--- fooccubus-prod/hack/scripts/succubus/influence.lua
+++ fooccubus-prod/hack/scripts/succubus/influence.lua
@@ -1,7 +1,7 @@
 -- Modify attributes based on a set of actions
 
 if not dfhack.isMapLoaded() then qerror('Map is not loaded.') end
-if not ... then qerror('Please enter a creature ID.') end
+if not ... then qerror('Please enter a creature ID and a domain.') end
 
 local args = {...}
 local unit, domain
@@ -13,15 +13,24 @@ domain = args[1]
 unit = df.unit.find(tonumber(args[2]))
 
 local function wrath(unit)
-
 	unit.status.current_soul.traits.ANGER = math.min(100, unit.status.current_soul.traits.ANGER + 20)
-	unit.status.current_soul.traits.LIBERALISM = math.min(100, unit.status.current_soul.traits.ANGER + 10)
-	unit.status.current_soul.traits.ALTRUISM = math.max(0, unit.status.current_soul.traits.ANGER - 10)
+	unit.status.current_soul.traits.LIBERALISM = math.min(100, unit.status.current_soul.traits.LIBERALISM + 10)
+	unit.status.current_soul.traits.ALTRUISM = math.max(0, unit.status.current_soul.traits.ALTRUISM - 10)
+end
 
+local function lust(unit)
+	unit.status.current_soul.traits.IMMODERATION = math.min(100, unit.status.current_soul.traits.IMMODERATION + 20)
+	unit.status.current_soul.traits.EXCITEMENT_SEEKING = math.min(100, unit.status.current_soul.traits.EXCITEMENT_SEEKING + 20)
+	unit.status.current_soul.traits.GREGARIOUSNESS = math.min(100, unit.status.current_soul.traits.GREGARIOUSNESS + 10)
+	unit.status.current_soul.traits.ALTRUISM = math.min(100, unit.status.current_soul.traits.ALTRUISM + 10)
+	unit.status.current_soul.traits.FRIENDLINESS = math.min(100, unit.status.current_soul.traits.FRIENDLINESS + 10)
+	-- todo add lover
 end
 
 if domain == 'wrath' then
 	wrath(unit)
+elseif domain == 'lust' then
+	lust(unit)
 else
 	qerror('Influence: Wrong domain entered')
 end
