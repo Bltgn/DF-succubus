1ba4168c36fc341ac4be53bfbe62d715a8f9582c
Guillaume <contact@devduweb.com>
2014-03-15 13:31:45 +0100

    scripts - updated influence

---
+++ fooccubus-prod/hack/scripts/succubus/forget-death.lua
@@ -0,0 +1,56 @@
+-- Citizens forget then recent deaths (pet/friend/relative...), depending of the relations of the source unit
+
+if not dfhack.isMapLoaded() then qerror('Map is not loaded.') end
+if not ... then qerror('forget-death : Missing parameters.') end
+
+local ratingMultiplier = 3 -- From 3 to 16 min-thoughts removed by this script
+local cFixedToughts = 0
+local debug = false
+
+-- Returns a rating from 1 to 5
+local function rating(unit)
+	return max.min(math.ceil(#unit.relations / 5), 5)
+end
+
+-- Chances of siege from the deep succubi
+local function payback()
+	if math.random(0, 100) <= 10 then
+		dfhack.run_script('force', 'siege', 'CULTS')
+		dfhack.gui.showAnnouncement("Your pride has attracted some unwanted attention!", COLOR_LIGHTRED, true)
+	end
+end
+
+-- Action
+unit = df.unit.find(tonumber(unitId))
+if not unit then qerror('forget-death : Unit not found.') end
+
+local cMaxToughts = rating(unit) * ratingMultiplier
+
+for fnUnitCount, fnUnit in ipairs(df.global.world.units.all) do
+	if fnUnit.race == df.global.ui.race_id then
+		local events = fnUnit.status.recent_events
+		for k,v in pairs(events) do
+			if v.type == df.unit_thought_type.WitnessDeath
+				or v.type == df.unit_thought_type.LostPet
+				or v.type == df.unit_thought_type.LostPet2
+				or v.type == df.unit_thought_type.RageKill
+				or v.type == df.unit_thought_type.SparringAccident
+				or v.type == df.unit_thought_type.LostSpouse
+				or v.type == df.unit_thought_type.LostFriend
+				or v.type == df.unit_thought_type.LostSibling
+				or v.type == df.unit_thought_type.LostChild
+				or v.type == df.unit_thought_type.LostMother
+				or v.type == df.unit_thought_type.LostFather
+			then
+				events:erase(k)
+				cFixedToughts = cFixedToughts + 1
+			end
+		end
+
+		if cFixedToughts >= cMaxToughts then
+			break
+		end
+	end
+end
+if debug then print("Death thought removed: "..total_fixed) end
+payback()
