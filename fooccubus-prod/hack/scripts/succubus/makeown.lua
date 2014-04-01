bb95a17cb11c29d211a17bcab3483c92052171a1
Guillaume Olivetti <contact@devduweb.com>
2014-04-01 20:16:21 +0200

    script - makeown does not work if the target is an enemy

--- fooccubus-prod/hack/scripts/succubus/makeown.lua
+++ fooccubus-prod/hack/scripts/succubus/makeown.lua
@@ -1,16 +1,20 @@
 -- Sets the units within line of sight of the unit
 --[[
 
-	This script is called by the conversion dens
+	This script is called by the conversion dens.
+	It will perform makeown on the visible units and add a makecitizen for good measure.
+	It will also remove flags related to invasions.
+	These units should be ready to act as citizens, if they are of the same race of your fort.
+
 	@author Boltgun
 
-	@todo support for semi megabeasts such as minotaur
 ]]
-local fov = require 'fov'
-local mo = require 'makeown'
-
 if not dfhack.isMapLoaded() then qerror('Map is not loaded.') end
 if not ... then qerror('Please enter a creature ID.') end
+
+local fov = require 'fov'
+local mo = require 'makeown'
+local utils = require 'utils'
 
 local unit, creatureSet
 local args = {...}
@@ -44,10 +48,34 @@ function validateCoords(unit, view)
 
 end
 
+-- Erase the enemy links
+function clearEnemy(unit)
+	hf = utils.binsearch(df.global.world.history.figures, unit.hist_figure_id, 'id')
+	for k, v in ipairs(hf.entity_links) do
+		if df.histfig_entity_link_enemyst:is_instance(v) and 
+			(v.entity_id == df.global.ui.civ_id or v.entity_id == df.global.ui.group_id) 
+		then
+			newLink = df.histfig_entity_link_former_prisonerst:new()
+			newLink.entity_id = v.entity_id
+			newLink.link_strength = v.link_strength
+			hf.entity_links[k] = newLink
+			v:delete()
+			print('deleted enemy link')
+		end
+	end
+
+	-- Make DF forget about the calculated enemies (ported from fix/loyaltycascade)
+	if not (unit.enemy.enemy_status_slot == -1) then
+		i = unit.enemy.enemy_status_slot
+		unit.enemy.enemy_status_slot = -1
+		print('enemy cache removed')
+	end
+end
+
 -- Find targets within the LOS of the creature
 function findLos(unitSource)
 	local view = fov.get_fov(10, unitSource.pos)
-	local i
+	local i, hf, k, v
 	local unitList = df.global.world.units.active
 
 	-- Check through the list for the right units
@@ -55,10 +83,7 @@ function findLos(unitSource)
 		unitTarget = unitList[i]
 		if isSelected(unitTarget, view) then
 			mo.make_own(unitTarget)
-
-			if makeCitizen then
-				mo.make_citizen(unitTarget)
-			end
+			mo.make_citizen(unitTarget)
 
 			-- Taking down all the hostility flags
 			unitTarget.flags1.marauder = false
@@ -71,6 +96,10 @@ function findLos(unitSource)
 			unitTarget.flags2.underworld = false
 			unitTarget.flags2.visitor_uninvited = false
 			unitTarget.invasion_id = -1
+			unit.relations.group_leader_id = -1
+			unit.relations.last_attacker_id = -1
+
+			clearEnemy(unitTarget)
 		end
 	end
 end
@@ -80,16 +109,13 @@ unit = df.unit.find(tonumber(args[1]))
 if not unit then qerror('Unit not found.') end
 
 -- Return the set of affected units
-makeCitizen = false
 if not args[2] then qerror('Please enter a creature set.') end
 if args[2] == 'invaders-deep' then
 	creatureSet = {['HUMAN'] = true, ['KOBOLD'] = true, ['ELF'] = true, ['DWARF'] = true, ['GOBLIN'] = true, ['FOOCCUBUS'] = true}
 elseif args[2] == 'invaders' then
 	creatureSet = {['HUMAN'] = true, ['KOBOLD'] = true, ['ELF'] = true, ['DWARF'] = true, ['GOBLIN'] = true, ['FOOCCUBUS_DEEP'] = true}
-	makeCitizen = true
 elseif args[2] == 'minotaur' then
 	creatureSet = {['MINOTAUR'] = true}
-	makeCitizen = true
 else
 	qerror('Unsupported creature set.')
 end
